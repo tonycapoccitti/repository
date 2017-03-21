6abd1f4c19ca14e78d39e95dbf2904f85e9ba9fd
metropolicon <telexxingou@hotmail.com>
2017-03-21 16:35:27 +0100

    mise a jour

---
+++ update_repo.bat
@@ -0,0 +1,6 @@
+C:\Users\HTPC\AppData\Local\Programs\Python\Python36\python.exe update_repo.py d:\IconMix\skins-addons\iconmixtools d:\IconMix\skins-addons\skin.iconmix-jarvis d:\IconMix\skins-addons\skin.iconmix-krypton 
+git add .
+git commit -a -m "mise a jour"
+git push
+
+pause
